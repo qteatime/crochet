c525f87e1692f481b2d56d548fc3e7001d21cac1
Q <hello@qteati.me>
2021-02-02 05:10:49 +0100

    Parse tree -> AST

---
+++ source/test.ts
@@ -0,0 +1,13 @@
+import * as Fs from "fs";
+import * as Util from "util";
+import { parse } from "./syntax/parser"
+
+function show(obj: any) {
+  return Util.inspect(obj, false, null, true);
+}
+
+const [file] = process.argv.slice(2);
+const source = Fs.readFileSync(file, "utf-8");
+const ast = parse(source);
+console.log(`${file}\n${"-".repeat(file.length)}`);
+console.log(show(ast));
