0d4cbecd9362358e9e42d61059e634db1fe504a8
Q <hello@qteati.me>
2021-03-04 05:57:22 +0100

    Partial functions and application

--- source/runtime/primitives/core-ops.ts
+++ source/runtime/primitives/core-ops.ts
@@ -1,7 +1,26 @@
+import { cast } from "../../utils";
+import { PartialExpr } from "../ir";
 import { Value } from "../logic/constraint";
-import { ErrUnexpectedType, Machine, _throw } from "../vm";
+import {
+  cvalue,
+  ErrInvalidArity,
+  ErrNoBranchMatched,
+  ErrUnexpectedType,
+  Machine,
+  State,
+  _push,
+  _throw,
+} from "../vm";
+import { ProcedureBranch } from "./procedure";
 import { CrochetType } from "./types";
-import { bfalse, btrue, CrochetRecord, CrochetValue } from "./value";
+import {
+  bfalse,
+  btrue,
+  CrochetPartial,
+  CrochetRecord,
+  CrochetValue,
+  PartialValue,
+} from "./value";
 
 export function from_bool(x: boolean): CrochetValue {
   return x ? btrue : bfalse;
@@ -12,5 +31,42 @@ export async function* safe_cast(x: any, type: CrochetType): Machine {
     return x;
   } else {
     return yield _throw(new ErrUnexpectedType(type, x));
+  }
+}
+
+export async function* invoke(
+  state: State,
+  name: string,
+  args: CrochetValue[]
+): Machine {
+  const procedure = state.world.procedures.lookup(name);
+  const branch0 = procedure.select(args);
+  let branch: ProcedureBranch;
+  if (branch0 == null) {
+    branch = cast(
+      yield _throw(new ErrNoBranchMatched(procedure, args)),
+      ProcedureBranch
+    );
+  } else {
+    branch = branch0;
+  }
+  const result = cvalue(yield _push(branch.procedure.invoke(state, args)));
+  return result;
+}
+
+export async function* apply(
+  state: State,
+  fn: CrochetPartial,
+  args: PartialValue[]
+) {
+  if (fn.arity !== args.length) {
+    return _throw(new ErrInvalidArity(fn, args.length));
+  } else {
+    const new_fn = fn.merge(args);
+    if (new_fn.is_saturated) {
+      return yield _push(invoke(state, new_fn.name, new_fn.concrete_args));
+    } else {
+      return new_fn;
+    }
   }
 }
--- source/runtime/primitives/value.ts
+++ source/runtime/primitives/value.ts
@@ -1,9 +1,11 @@
-import { every, zip } from "../../utils/utils";
+import { cast, every, zip } from "../../utils/utils";
 import { Machine } from "../vm";
+import { Environment } from "../world";
 import {
   CrochetRole,
   CrochetType,
   TCrochetEnum,
+  TCrochetPartial,
   TCrochetType,
   tFalse,
   tInteger,
@@ -315,6 +317,90 @@ export class CrochetUnknown extends CrochetValue {
 
   to_text() {
     return `<unknown>`;
+  }
+}
+
+export class CrochetPartial extends CrochetValue {
+  readonly type: TCrochetPartial;
+
+  constructor(
+    readonly name: string,
+    readonly env: Environment,
+    readonly values: PartialValue[]
+  ) {
+    super();
+    this.type = new TCrochetPartial(name);
+  }
+
+  has_role(role: CrochetRole): boolean {
+    return false;
+  }
+
+  equals(other: CrochetValue): boolean {
+    return other === this;
+  }
+
+  as_bool(): boolean {
+    return true;
+  }
+  to_js() {
+    return this;
+  }
+
+  to_text(): string {
+    return `<partial ${this.name}>`;
+  }
+
+  merge(candidates: PartialValue[]) {
+    const values = this.values.slice();
+    let current_index = 0;
+    let candidate_index = 0;
+
+    while (
+      candidate_index < candidates.length &&
+      current_index < values.length
+    ) {
+      const candidate = candidates[candidate_index];
+      const current = values[current_index];
+
+      if (current instanceof PartialConcrete) {
+        current_index += 1;
+      } else if (current instanceof PartialHole) {
+        values[current_index] = candidate;
+        candidate_index += 1;
+        current_index += 1;
+      }
+    }
+
+    if (candidate_index < candidates.length) {
+      throw new Error(`Invalid arity`);
+    } else {
+      return new CrochetPartial(this.name, this.env, values);
+    }
+  }
+
+  get is_saturated() {
+    return this.values.every((x) => x instanceof PartialConcrete);
+  }
+
+  get arity() {
+    return partial_holes(this.values);
+  }
+
+  get concrete_args() {
+    return this.values.map((x) => cast(x, PartialConcrete).value);
+  }
+}
+
+export function partial_holes(values: PartialValue[]) {
+  return values.filter((x) => x instanceof PartialHole).length;
+}
+
+export abstract class PartialValue {}
+export class PartialHole extends PartialValue {}
+export class PartialConcrete extends PartialValue {
+  constructor(readonly value: CrochetValue) {
+    super();
   }
 }
 
