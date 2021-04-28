4d323a6815f77b106846717b1061937b48237097
Q <hello@qteati.me>
2021-04-28 20:18:38 +0200

    Logic IR wip

---
+++ source/ir/logic.ts
@@ -0,0 +1,136 @@
+import { Pattern } from "./pattern";
+import { Type } from "./type";
+import { BasicBlock } from "./expression";
+
+type Metadata = number;
+type uint32 = number;
+
+export enum PredicateTag {
+  AND,
+  OR,
+  NOT,
+  RELATION,
+  SAMPLE_RELATION,
+  SAMPLE_TYPE,
+  CONSTRAIN,
+  LET,
+  TYPE,
+  ALWAYS,
+}
+
+export type Predicate = PAnd;
+
+export abstract class PredicateBase {}
+
+export class PAnd extends PredicateBase {
+  readonly tag = PredicateTag.AND;
+
+  constructor(
+    readonly meta: Metadata,
+    readonly left: Predicate,
+    readonly right: Predicate
+  ) {
+    super();
+  }
+}
+
+export class POr extends PredicateBase {
+  readonly tag = PredicateTag.OR;
+
+  constructor(
+    readonly meta: Metadata,
+    readonly left: Predicate,
+    readonly right: Predicate
+  ) {
+    super();
+  }
+}
+
+export class PNot extends PredicateBase {
+  readonly tag = PredicateTag.NOT;
+
+  constructor(readonly meta: Metadata, readonly pred: Predicate) {
+    super();
+  }
+}
+
+export class PRelation extends PredicateBase {
+  readonly tag = PredicateTag.RELATION;
+
+  constructor(
+    readonly meta: Metadata,
+    readonly relation: string,
+    readonly patterns: Pattern[]
+  ) {
+    super();
+  }
+}
+
+export class PSampleRelation extends PredicateBase {
+  readonly tag = PredicateTag.SAMPLE_RELATION;
+
+  constructor(
+    readonly meta: Metadata,
+    readonly size: uint32,
+    readonly relation: string,
+    readonly patterns: Pattern[]
+  ) {
+    super();
+  }
+}
+
+export class PSampleType extends PredicateBase {
+  readonly tag = PredicateTag.SAMPLE_TYPE;
+
+  constructor(
+    readonly meta: Metadata,
+    readonly size: uint32,
+    readonly type: Type
+  ) {
+    super();
+  }
+}
+
+export class PConstrained extends PredicateBase {
+  readonly tag = PredicateTag.CONSTRAIN;
+
+  constructor(
+    readonly meta: Metadata,
+    readonly predicate: Predicate,
+    readonly constraint: BasicBlock
+  ) {
+    super();
+  }
+}
+
+export class PLet extends PredicateBase {
+  readonly tag = PredicateTag.LET;
+
+  constructor(
+    readonly meta: Metadata,
+    readonly name: string,
+    readonly value: BasicBlock
+  ) {
+    super();
+  }
+}
+
+export class PType extends PredicateBase {
+  readonly tag = PredicateTag.LET;
+
+  constructor(
+    readonly meta: Metadata,
+    readonly name: string,
+    readonly type: Type
+  ) {
+    super();
+  }
+}
+
+export class PAlways extends PredicateBase {
+  readonly tag = PredicateTag.ALWAYS;
+
+  constructor(readonly meta: Metadata) {
+    super();
+  }
+}
---
+++ source/ir/pattern.ts
@@ -0,0 +1,70 @@
+import { Type } from "./type";
+import { Literal } from "./literal";
+
+type Metadata = number;
+type uint32 = number;
+
+export enum PatternTag {
+  HAS_TYPE,
+  GLOBAL,
+  VARIABLE,
+  SELF,
+  WILDCARD,
+  LITERAL,
+}
+
+export type Pattern = TypePattern;
+
+export abstract class PatternBase {}
+
+export class TypePattern extends PatternBase {
+  readonly tag = PatternTag.HAS_TYPE;
+
+  constructor(
+    readonly meta: Metadata,
+    readonly type: Type,
+    readonly pattern: Pattern
+  ) {
+    super();
+  }
+}
+
+export class LiteralPattern extends PatternBase {
+  readonly tag = PatternTag.LITERAL;
+
+  constructor(readonly meta: Metadata, readonly literal: Literal) {
+    super();
+  }
+}
+
+export class GlobalPattern extends PatternBase {
+  readonly tag = PatternTag.GLOBAL;
+
+  constructor(readonly meta: Metadata, readonly name: string) {
+    super();
+  }
+}
+
+export class SelfPattern extends PatternBase {
+  readonly tag = PatternTag.SELF;
+
+  constructor(readonly meta: Metadata) {
+    super();
+  }
+}
+
+export class VariablePattern extends PatternBase {
+  readonly tag = PatternTag.VARIABLE;
+
+  constructor(readonly meta: Metadata, readonly name: string) {
+    super();
+  }
+}
+
+export class WildcardPattern extends PatternBase {
+  readonly tag = PatternTag.WILDCARD;
+
+  constructor(readonly meta: Metadata) {
+    super();
+  }
+}
