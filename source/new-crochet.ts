97ec22857db69c68eb307369210eec44add0dc46
Q <hello@qteati.me>
2021-05-01 17:37:11 +0200

    Expose more commands for testing

--- source/new-crochet.ts
+++ source/new-crochet.ts
@@ -10,26 +10,64 @@ import { logger } from "./utils/logger";
 
 export { IR, Compiler, Binary, VM, Package };
 
+function read_crochet(file: string) {
+  const source = FS.readFileSync(file, "utf-8");
+  const ast = Compiler.parse(source, file);
+  const ir = Compiler.lowerToIR(file, source, ast);
+  return ir;
+}
+
+function read_binary(file: string) {
+  const source = FS.readFileSync(file);
+  const ir = Binary.decode_program(source);
+  return ir;
+}
+
+function read(file: string) {
+  switch (Path.extname(file)) {
+    case ".crochet":
+      return read_crochet(file);
+    case ".croc":
+      return read_binary(file);
+    default:
+      throw Error(`Unsupported file ${file}`);
+  }
+}
+
 void (async function main() {
   try {
-    const [file, verbose] = process.argv.slice(2);
+    const [command, file, verbose] = process.argv.slice(2);
     logger.verbose = !!verbose;
+    if (!file) {
+      throw new Error(`Usage: new-crochet <run|compile> <file> [verbose]`);
+    }
 
-    debugger;
-    const source = FS.readFileSync(file, "utf-8");
-    const ast = Compiler.parse(source, file);
-    const ir = Compiler.lowerToIR(file, source, ast);
+    switch (command) {
+      case "compile": {
+        const source = FS.readFileSync(file, "utf-8");
+        const ast = Compiler.parse(source, file);
+        const ir = Compiler.lowerToIR(file, source, ast);
+        const target = FS.createWriteStream(file + ".croc");
+        Binary.encode_program(ir, target);
+        console.log("Generated ", file + ".croc");
+        return;
+      }
 
-    const universe = VM.make_universe();
-    const pkg = new VM.CrochetPackage(universe.world, "(anonymous)", file);
-    const module = VM.load_module(universe, pkg, ir);
+      case "run": {
+        const ir = read(file);
 
-    debugger;
-    const value = await VM.run_command(universe, module, "main: _", [
-      VM.Values.make_tuple(universe, []),
-    ]);
+        const universe = VM.make_universe();
+        const pkg = new VM.CrochetPackage(universe.world, "(anonymous)", file);
+        const module = VM.load_module(universe, pkg, ir);
 
-    console.log(VM.Location.simple_value(value));
+        const value = await VM.run_command(universe, module, "main: _", [
+          VM.Values.make_tuple(universe, []),
+        ]);
+
+        console.log(VM.Location.simple_value(value));
+        return;
+      }
+    }
   } catch (e) {
     console.error(e.stack);
     process.exit(1);
