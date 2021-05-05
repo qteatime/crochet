d4b2cb7afdc0345575da3e726275d65fee258314
Q <hello@qteati.me>
2021-05-05 20:48:48 +0200

    Some progress in rewriting natives

--- stdlib/core/native/floats.ts
+++ stdlib/core/native/floats.ts
@@ -1,4 +1,4 @@
-import type { ForeignInterface } from "../../../source/crochet";
+import type { ForeignInterface } from "../../../build/crochet";
 
 export default (ffi: ForeignInterface) => {
   const nan = ffi.float(NaN);
