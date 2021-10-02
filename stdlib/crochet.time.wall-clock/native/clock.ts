fc7fd235b56cb97c08dea0059efeca99f1245eca
Q <hello@qteati.me>
2021-10-02 15:45:36 +0200

    Move wall-clock into crochet.time

--- stdlib/crochet.time.wall-clock/native/clock.ts
+++
@@ -1,7 +0,0 @@
-import type { ForeignInterface } from "../../../build/crochet";
-
-export default (ffi: ForeignInterface) => {
-  ffi.defun("clock.now", () => {
-    return ffi.integer(BigInt(new Date().getTime()));
-  });
-};
