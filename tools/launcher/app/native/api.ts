9853296e2ba5f7c846917be87260c59459ec15f8
Q <hello@qteati.me>
2021-10-09 16:42:36 +0200

    examples

---
+++ tools/launcher/app/native/api.ts
@@ -0,0 +1,13 @@
+import type { ForeignInterface, CrochetValue } from "../../../../build/crochet";
+
+export default (ffi: ForeignInterface) => {
+  async function get_text(url: string) {
+    const response = await fetch(url);
+    const json = await response.text();
+    return ffi.text(json);
+  }
+
+  ffi.defmachine("api.examples", function* () {
+    return yield ffi.await(get_text("/api/examples"));
+  });
+};
---
+++ tools/launcher/source/api.ts
@@ -0,0 +1,19 @@
+import * as FS from "fs";
+import * as Path from "path";
+import * as Glob from "glob";
+
+function read_pkg(file: string) {
+  return JSON.parse(FS.readFileSync(file, "utf-8"));
+}
+
+export class API {
+  constructor(readonly root: string) {}
+
+  examples() {
+    const pkgs_json = Glob.sync("**/crochet.json", {
+      cwd: Path.join(this.root, "examples"),
+      absolute: true,
+    });
+    return pkgs_json.map((x) => ({ filename: x, meta: read_pkg(x) }));
+  }
+}
