b539752998b598fc96ac63a7de574e810842b973
Q <hello@qteati.me>
2021-10-21 21:05:21 +0200

    wip

--- stdlib/crochet.debug/native/debug.ts
+++ stdlib/crochet.debug/native/debug.ts
@@ -22,6 +22,19 @@ export function format_time_diff(n: bigint) {
 }
 
 export default (ffi: ForeignInterface) => {
+  const is_node = typeof process != "undefined";
+
+  function now() {
+    if (is_node) {
+      return process.hrtime.bigint();
+    } else {
+      const us = performance.now();
+      const integral = Math.trunc(us) * 1000;
+      const fractional = Math.round((us - Math.trunc(us)) * 1000);
+      return BigInt(integral) + BigInt(fractional);
+    }
+  }
+
   ffi.defmachine("debug.write", function* (text) {
     yield ffi.push_transcript("debug", ffi.text_to_string(text));
     return ffi.nothing;
@@ -33,10 +46,10 @@ export default (ffi: ForeignInterface) => {
   });
 
   ffi.defmachine("debug.time", function* (label, computation) {
-    const ns_start = process.hrtime.bigint();
+    const us_start = now();
     const result = yield ffi.apply(computation, []);
-    const ns_end = process.hrtime.bigint();
-    const diff = ns_end - ns_start;
+    const us_end = now();
+    const diff = us_end - us_start;
     yield ffi.push_transcript(
       "time",
       `[${ffi.text_to_string(label)}] ${format_time_diff(diff)}`
