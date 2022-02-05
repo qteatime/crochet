464a008063c07ccbad7526d4c69bc0d703eba21e
Q <hello@qteati.me>
2022-02-06 00:40:12 +0100

    Custom parsing, separate extended json

--- stdlib/crochet.language.json/native/json.ts
+++ stdlib/crochet.language.json/native/json.ts
@@ -78,7 +78,39 @@ export default (ffi: ForeignInterface) => {
     }
   }
 
-  const _null = ffi.box(new JsonNull());
+  const _null = new JsonNull();
+
+  function make_reify(extended: boolean) {
+    function reify_json(key: string, value: unknown) {
+      if (value instanceof Json) {
+        return value;
+      } else if (value == null) {
+        return _null;
+      } else if (typeof value === "number") {
+        return new JsonNumber(value);
+      } else if (typeof value === "boolean") {
+        return new JsonBoolean(value);
+      } else if (typeof value === "string") {
+        return new JsonText(value);
+      } else if (Array.isArray(value)) {
+        return new JsonList(value);
+      } else if (extended && typeof value === "object" && "@type" in value) {
+        if (!((value as any)["@type"] instanceof JsonText)) {
+          throw ffi.panic("invalid-type", "expected text");
+        }
+        if (!("value" in value)) {
+          throw ffi.panic("invalid-type", "expected a proper typed json");
+        }
+        const type = ((value as any)["@type"] as JsonText).value;
+        const v = (value as any).value;
+        return new JsonTyped(type, v);
+      } else {
+        return new JsonRecord(Object.entries(value as any));
+      }
+    }
+
+    return reify_json;
+  }
 
   ffi.defun("json.typed", (tag, value) => {
     return ffi.box(
@@ -87,7 +119,7 @@ export default (ffi: ForeignInterface) => {
   });
 
   ffi.defun("json.null", () => {
-    return _null;
+    return ffi.box(_null);
   });
 
   ffi.defun("json.boolean", (x) => {
@@ -119,45 +151,67 @@ export default (ffi: ForeignInterface) => {
     );
   });
 
-  function to_json(x: unknown): unknown {
-    if (typeof x === "bigint") {
-      return Number(x);
-    } else if (Array.isArray(x)) {
-      return x.map(to_json);
-    } else if (x instanceof Map) {
-      const value = Object.create(null);
-      for (const [k, v] of x.entries()) {
-        value[k] = to_json(v);
-      }
-      return value;
-    } else {
-      return x;
-    }
-  }
-
-  function from_json(x: unknown): unknown {
-    if (Array.isArray(x)) {
-      return x.map((a) => from_json(a));
-    } else if (typeof x === "object" && x != null) {
-      const result = new Map<string, unknown>();
-      for (const [k, v] of Object.entries(x)) {
-        result.set(k, from_json(v));
-      }
-      return result;
-    } else {
-      return x;
-    }
-  }
-
   ffi.defun("json.untrusted", (text) => {
     return ffi.untrusted_text(ffi.text_to_string(text));
   });
 
-  ffi.defun("json.parse", (text, trusted) => {
-    return ffi.from_plain_native(
-      from_json(JSON.parse(ffi.text_to_string(text))),
-      ffi.to_js_boolean(trusted)
+  ffi.defun("json.parse", (text, extended0) => {
+    const extended = ffi.to_js_boolean(extended0);
+    const value = JSON.parse(ffi.text_to_string(text), make_reify(extended));
+    return ffi.box(value);
+  });
+
+  ffi.defun("json.get-type", (x0) => {
+    const x = ffi.unbox_typed(Json, x0);
+    if (x instanceof JsonNull) {
+      return ffi.text("null");
+    } else if (x instanceof JsonNumber) {
+      return ffi.text("number");
+    } else if (x instanceof JsonText) {
+      return ffi.text("text");
+    } else if (x instanceof JsonBoolean) {
+      return ffi.text("boolean");
+    } else if (x instanceof JsonList) {
+      return ffi.text("list");
+    } else if (x instanceof JsonRecord) {
+      return ffi.text("record");
+    } else if (x instanceof JsonTyped) {
+      return ffi.text("typed");
+    } else {
+      throw ffi.panic("invalid-type", "invalid json type");
+    }
+  });
+
+  ffi.defun("json.get-number", (x) => {
+    return ffi.float_64(ffi.unbox_typed(JsonNumber, x).value);
+  });
+
+  ffi.defun("json.get-boolean", (x) => {
+    return ffi.boolean(ffi.unbox_typed(JsonBoolean, x).value);
+  });
+
+  ffi.defun("json.get-text", (x) => {
+    return ffi.text(ffi.unbox_typed(JsonText, x).value);
+  });
+
+  ffi.defun("json.get-list", (x) => {
+    return ffi.list(ffi.unbox_typed(JsonList, x).values.map((x) => ffi.box(x)));
+  });
+
+  ffi.defun("json.get-record-entries", (x) => {
+    return ffi.list(
+      ffi.unbox_typed(JsonRecord, x).entries.map(([k, v]) => {
+        return ffi.list([ffi.text(k), ffi.box(v)]);
+      })
     );
+  });
+
+  ffi.defun("json.get-typed-tag", (x) => {
+    return ffi.text(ffi.unbox_typed(JsonTyped, x).tag);
+  });
+
+  ffi.defun("json.get-typed-value", (x) => {
+    return ffi.box(ffi.unbox_typed(JsonTyped, x).value);
   });
 
   ffi.defun("json.serialise", (value, trusted) => {
@@ -171,7 +225,7 @@ export default (ffi: ForeignInterface) => {
   });
 
   ffi.defun("json.pretty-print", (value, indent, trusted) => {
-    const json = to_json(ffi.to_plain_native(value));
+    const json = ffi.unbox_typed(Json, value);
     const json_text = JSON.stringify(
       json,
       null,
