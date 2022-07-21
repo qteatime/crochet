e48bef8cce540730e0bbb0c144d8f130e8375b35
Q <hello@qteati.me>
2022-07-21 23:55:20 +0200

    Simple menus

--- stdlib/crochet.novella/native/dom.ts
+++ stdlib/crochet.novella/native/dom.ts
@@ -16,6 +16,54 @@ export default (ffi: ForeignInterface) => {
     return p;
   }
 
+  function h(
+    tag: string,
+    attrs: { [key: string]: string },
+    children: (string | Node)[]
+  ) {
+    const element = document.createElement(tag);
+    for (const [key, value] of Object.entries(attrs)) {
+      element.setAttribute(key, value);
+    }
+    for (const child of children) {
+      element.append(child);
+    }
+    return element;
+  }
+
+  function wrap(x0: number, max: number) {
+    const x = x0 % max;
+    return x < 0 ? max + x : x;
+  }
+
+  function wrap_choose<A>(x: A[], index0: number) {
+    const index = wrap(index0, x.length);
+    return x[index];
+  }
+
+  function clear(x: HTMLElement) {
+    x.textContent = "";
+    return x;
+  }
+
+  abstract class MenuItem {
+    abstract render(): HTMLElement;
+  }
+
+  class MILeaf extends MenuItem {
+    constructor(readonly text: string, readonly value: CrochetValue) {
+      super();
+    }
+
+    render() {
+      return h("div", { class: "novella-menu-item-text" }, [this.text]);
+    }
+  }
+
+  class Menu {
+    constructor(readonly items: MenuItem[]) {}
+  }
+
   ffi.defun("dom.make", (tag, klass) => {
     const element = document.createElement(ffi.text_to_string(tag));
     element.className = ffi.text_to_string(klass);
@@ -98,5 +146,99 @@ export default (ffi: ForeignInterface) => {
     yield ffi.await(deferred.promise.then((_) => ffi.nothing));
 
     return ffi.nothing;
+  });
+
+  ffi.defun("dom.make-menu-leaf", (text, value) => {
+    return ffi.box(new MILeaf(ffi.text_to_string(text), value));
+  });
+
+  ffi.defun("dom.make-menu", (items0) => {
+    const items = ffi
+      .list_to_array(items0)
+      .map((x) => ffi.unbox_typed(MenuItem, x));
+    return ffi.box(new Menu(items));
+  });
+
+  ffi.defmachine("dom.show-menu", function* (prefix0, node0, menu0) {
+    const prefix = ffi.text_to_string(prefix0);
+    const node = ffi.unbox_typed(HTMLElement, node0);
+    const menu = ffi.unbox_typed(Menu, menu0);
+    const deferred = defer<CrochetValue>();
+    let items = menu.items;
+    let position = 0;
+
+    const menu_container = h("div", { class: "novella-menu-container" }, []);
+    const menu_prefix = h("div", { class: "novella-menu-prefix" }, [prefix]);
+    const menu_selection = h(
+      "div",
+      { class: "novella-menu-selection-container" },
+      []
+    );
+    const menu_selection_previous = h(
+      "div",
+      { class: "novella-menu-selection-previous" },
+      []
+    );
+    const menu_selection_current = h(
+      "div",
+      { class: "novella-menu-selection-current" },
+      []
+    );
+    const menu_selection_next = h(
+      "div",
+      { class: "novella-menu-selection-next" },
+      []
+    );
+    const menu_suffix = h("div", { class: "novella-menu-suffix" }, []);
+
+    menu_container.appendChild(menu_prefix);
+    menu_container.appendChild(menu_selection);
+    menu_selection.appendChild(menu_selection_previous);
+    menu_selection.appendChild(menu_selection_current);
+    menu_selection.appendChild(menu_selection_next);
+    menu_container.appendChild(menu_suffix);
+
+    function render_selection() {
+      const current = render_item(wrap_choose(items, position));
+      const previous = render_item(wrap_choose(items, position - 1));
+      const next = render_item(wrap_choose(items, position + 1));
+      clear(menu_selection_previous).appendChild(previous);
+      clear(menu_selection_current).appendChild(current);
+      clear(menu_selection_next).appendChild(next);
+    }
+
+    function render_item(item: MenuItem) {
+      return item.render();
+    }
+
+    function accept_selection(selected: MenuItem) {
+      if (selected instanceof MILeaf) {
+        document.removeEventListener("keyup", input_listener);
+        deferred.resolve(selected.value);
+      }
+    }
+
+    function input_listener(ev: KeyboardEvent) {
+      if (ev.key === "ArrowUp") {
+        position = wrap(position - 1, items.length);
+      } else if (ev.key === "ArrowDown") {
+        position = wrap(position + 1, items.length);
+      } else if (ev.key === "Enter") {
+        const selected = wrap_choose(items, position);
+        accept_selection(selected);
+        ev.stopPropagation();
+        ev.preventDefault();
+        return;
+      }
+      ev.stopPropagation();
+      ev.preventDefault();
+      render_selection();
+    }
+
+    render_selection();
+    node.appendChild(menu_container);
+    document.addEventListener("keyup", input_listener);
+
+    return yield ffi.await(deferred.promise);
   });
 };
