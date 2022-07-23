671c515a5d7917e7a1b6a2696d96f2abc3814513
Q <hello@qteati.me>
2022-07-23 13:00:38 +0200

    tree menu

--- stdlib/crochet.novella/native/dom.ts
+++ stdlib/crochet.novella/native/dom.ts
@@ -16,6 +16,13 @@ export default (ffi: ForeignInterface) => {
     return p;
   }
 
+  function* enumerate<A>(xs: A[]) {
+    let index = 0;
+    for (const x of xs) {
+      yield { index: index++, value: x };
+    }
+  }
+
   function h(
     tag: string,
     attrs: { [key: string]: string },
@@ -29,6 +36,23 @@ export default (ffi: ForeignInterface) => {
       element.append(child);
     }
     return element;
+  }
+
+  class HTMLWrapper {
+    constructor(readonly value: HTMLElement) {}
+
+    on(event: string, callback: (...x: any[]) => any) {
+      this.value.addEventListener(event, callback);
+      return this;
+    }
+
+    unwrap() {
+      return this.value;
+    }
+  }
+
+  function $(x: HTMLElement) {
+    return new HTMLWrapper(x);
   }
 
   function wrap(x0: number, max: number) {
@@ -47,7 +71,8 @@ export default (ffi: ForeignInterface) => {
   }
 
   abstract class MenuItem {
-    abstract render(): HTMLElement;
+    abstract render(): { text: HTMLElement; suffix: HTMLElement };
+    abstract suffix_text(): string[];
   }
 
   class MILeaf extends MenuItem {
@@ -56,7 +81,39 @@ export default (ffi: ForeignInterface) => {
     }
 
     render() {
-      return h("div", { class: "novella-menu-item-text" }, [this.text]);
+      return {
+        text: h("div", { class: "novella-menu-item-text" }, [this.text]),
+        suffix: h("div", { class: "novella-menu-item-empty" }, []),
+      };
+    }
+
+    suffix_text() {
+      return [this.text];
+    }
+  }
+
+  class MIBranch extends MenuItem {
+    constructor(readonly prefix: string, readonly items: MenuItem[]) {
+      super();
+    }
+
+    suffix_text() {
+      return this.items.flatMap((x) =>
+        x.suffix_text().map((x) => `${this.prefix} ${x}`)
+      );
+    }
+
+    render() {
+      return {
+        text: h("div", { class: "novella-menu-item-branch" }, [this.prefix]),
+        suffix: h("div", { class: "novella-menu-item-rotating-suffix" }, [
+          ...this.items
+            .flatMap((x) => x.suffix_text())
+            .map((x) =>
+              h("div", { class: "novella-menu-item-rotating-suffix-text" }, [x])
+            ),
+        ]),
+      };
     }
   }
 
@@ -152,6 +209,13 @@ export default (ffi: ForeignInterface) => {
     return ffi.box(new MILeaf(ffi.text_to_string(text), value));
   });
 
+  ffi.defun("dom.make-menu-branch", (prefix, items0) => {
+    const items = ffi
+      .list_to_array(items0)
+      .map((x) => ffi.unbox_typed(MenuItem, x));
+    return ffi.box(new MIBranch(ffi.text_to_string(prefix), items));
+  });
+
   ffi.defun("dom.make-menu", (items0) => {
     const items = ffi
       .list_to_array(items0)
@@ -165,10 +229,13 @@ export default (ffi: ForeignInterface) => {
     const menu = ffi.unbox_typed(Menu, menu0);
     const deferred = defer<CrochetValue>();
     let items = menu.items;
+    let trail: MIBranch[] = [];
     let position = 0;
+    let roll_timer: any = null;
 
     const menu_container = h("div", { class: "novella-menu-container" }, []);
     const menu_prefix = h("div", { class: "novella-menu-prefix" }, [prefix]);
+    const menu_breadcrumb = h("div", { class: "novella-menu-breadcrumb" }, []);
     const menu_selection = h(
       "div",
       { class: "novella-menu-selection-container" },
@@ -192,6 +259,7 @@ export default (ffi: ForeignInterface) => {
     const menu_suffix = h("div", { class: "novella-menu-suffix" }, []);
 
     menu_container.appendChild(menu_prefix);
+    menu_container.appendChild(menu_breadcrumb);
     menu_container.appendChild(menu_selection);
     menu_selection.appendChild(menu_selection_previous);
     menu_selection.appendChild(menu_selection_current);
@@ -199,40 +267,115 @@ export default (ffi: ForeignInterface) => {
     menu_container.appendChild(menu_suffix);
 
     function render_selection() {
-      const current = render_item(wrap_choose(items, position));
-      const previous = render_item(wrap_choose(items, position - 1));
-      const next = render_item(wrap_choose(items, position + 1));
+      const { text: current, suffix: current_suffix } = wrap_choose(
+        items,
+        position
+      ).render();
+      const previous = wrap_choose(items, position - 1).render().text;
+      const next = wrap_choose(items, position + 1).render().text;
+      clear(menu_breadcrumb);
+      for (const { index, value } of enumerate(trail)) {
+        menu_breadcrumb.appendChild(
+          h("div", { class: "novella-menu-breadcrumb-node" }, [
+            value.render().text,
+            $(
+              h("button", { class: "novella-menu-breadcrumb-remove" }, [
+                h("i", { class: "novella-icon fas fa-times" }, []),
+              ])
+            )
+              .on("click", (ev: MouseEvent) => {
+                if (index == 0) {
+                  trail = [];
+                  items = menu.items;
+                  position = 0;
+                  render_selection();
+                } else {
+                  trail = trail.slice(0, index);
+                  items = trail[index - 1].items;
+                  position = 0;
+                  render_selection();
+                }
+              })
+              .unwrap(),
+          ])
+        );
+      }
+
       clear(menu_selection_previous).appendChild(previous);
       clear(menu_selection_current).appendChild(current);
+      clear(menu_suffix).appendChild(current_suffix);
       clear(menu_selection_next).appendChild(next);
+      roll_animate();
     }
 
-    function render_item(item: MenuItem) {
-      return item.render();
+    function roll_animate() {
+      clearTimeout(roll_timer);
+      const items = Array.from(
+        menu_suffix.querySelectorAll(
+          ".novella-menu-item-rotating-suffix .novella-menu-item-rotating-suffix-text"
+        )
+      );
+      if (items.length === 0) {
+        return;
+      }
+
+      let current = 0;
+      function do_next() {
+        clearTimeout(roll_timer);
+
+        for (const { index, value } of enumerate(items)) {
+          if (index === current) {
+            value.classList.add("novella-rotating-suffix-shown");
+          } else {
+            value.classList.remove("novella-rotating-suffix-shown");
+          }
+        }
+        current = wrap(current + 1, items.length);
+
+        roll_timer = setTimeout(do_next, 1500);
+      }
+      do_next();
     }
 
     function accept_selection(selected: MenuItem) {
       if (selected instanceof MILeaf) {
         document.removeEventListener("keyup", input_listener);
         deferred.resolve(selected.value);
+      } else if (selected instanceof MIBranch) {
+        trail.push(selected);
+        items = selected.items;
+        position = 0;
+        render_selection();
       }
     }
 
     function input_listener(ev: KeyboardEvent) {
       if (ev.key === "ArrowUp") {
         position = wrap(position - 1, items.length);
+        ev.stopPropagation();
+        ev.preventDefault();
+        render_selection();
       } else if (ev.key === "ArrowDown") {
         position = wrap(position + 1, items.length);
+        ev.stopPropagation();
+        ev.preventDefault();
+        render_selection();
       } else if (ev.key === "Enter") {
         const selected = wrap_choose(items, position);
         accept_selection(selected);
         ev.stopPropagation();
         ev.preventDefault();
         return;
+      } else if (ev.key === "Backspace") {
+        trail.pop();
+        if (trail.length === 0) {
+          items = menu.items;
+        } else {
+          items = trail[trail.length - 1].items;
+        }
+        position = 0;
+        render_selection();
       }
-      ev.stopPropagation();
-      ev.preventDefault();
-      render_selection();
     }
 
     render_selection();
